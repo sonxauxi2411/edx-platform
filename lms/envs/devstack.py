f00ef0054edc9c253084ed80d6a85398bc8f1bd3
sonxauxi2411 <112046655+sonxauxi2411@users.noreply.github.com>
2023-06-08 08:56:41 +0700

    [ADD] Disable debug bar

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -109,10 +109,11 @@ DEBUG_TOOLBAR_CONFIG = {
 
 def should_show_debug_toolbar(request):  # lint-amnesty, pylint: disable=missing-function-docstring
     # We always want the toolbar on devstack unless running tests from another Docker container
-    hostname = request.get_host()
-    if hostname.startswith('edx.devstack.lms:') or hostname.startswith('lms.devstack.edx:'):
-        return False
-    return True
+    # hostname = request.get_host()
+    # if hostname.startswith('edx.devstack.lms:') or hostname.startswith('lms.devstack.edx:'):
+    #     return False
+    # return True
+    return False
 
 ########################### PIPELINE #################################
 
