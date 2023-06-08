d8880d22f7ead2aba5736aea35bbf273e0f9987b
sonxauxi2411 <112046655+sonxauxi2411@users.noreply.github.com>
2023-06-08 08:31:05 +0700

    [ADD] Add merged csv task

--- lms/djangoapps/instructor_tools/api.py
+++ lms/djangoapps/instructor_tools/api.py
@@ -6,7 +6,8 @@ from django.db import transaction
 from common.djangoapps.util.json_request import JsonResponse
 from lms.djangoapps.instructor_task import api
 from opaque_keys.edx.keys import CourseKey
-from lms.djangoapps.instructor_tools.all_grades_csv import get_all_csv_file
+from lms.djangoapps.instructor_tools.all_grades_csv import get_all_csv_file, process_grade_file, merge_all_csv, process_merged
+
 
 
 @transaction.non_atomic_requests
@@ -15,9 +16,18 @@ from lms.djangoapps.instructor_tools.all_grades_csv import get_all_csv_file
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)
 def calculate_all_grades_csv(request):
     # all_course = CourseOverview.get_all_courses(['FUNiX'])
-
+    all_course = CourseOverview.get_all_courses(['FUNiX'])
     # for course in all_course:
     #     course_key = CourseKey.from_string(str(course))
     #     api.submit_calculate_grades_csv_not_async(request, course_key)
-    print(get_all_csv_file())
-    return JsonResponse({"1": 1})
+    for course in all_course:
+        course_key = CourseKey.from_string(str(course))
+        api.submit_calculate_grades_csv_not_async(request, course_key)
+        
+    all_csv_files = get_all_csv_file()
+    all_grade_files = process_grade_file(all_csv_files)
+    combined_file = merge_all_csv(all_grade_files)
+    out_file = process_merged(combined_file)
+
+
+    return JsonResponse({'output': out_file})
